8597c2221f4c3a19ded315c2c93e9b8309d8d836
Clif Smith <clif@spanning.com>
2014-11-03 07:54:04 -0600

    Include the git::default recipe

--- metadata.rb
+++ metadata.rb
@@ -2,6 +2,6 @@ maintainer        "Clif Smith"
 maintainer_email  "clif@texicans.us"
 license           "Apache 2.0"
 description       "Deploys the check_elasticsearch Nagios plugin"
-version           "1.0.0"
+version           "1.0.1"
 
 depends "git"
