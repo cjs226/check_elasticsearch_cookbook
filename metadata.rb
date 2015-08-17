0f3e0117a95c1e4b2573f1cfae161a850a1dce60
Clif Smith <clif@spanning.com>
2015-08-17 09:42:33 -0500

    Updated metadata ver

--- metadata.rb
+++ metadata.rb
@@ -3,6 +3,6 @@ maintainer        "Clif Smith"
 maintainer_email  "clif@texicans.us"
 license           "Apache 2.0"
 description       "Deploys the check_elasticsearch Nagios plugin"
-version           "1.0.1"
+version           "1.0.2"
 
 depends "git"
